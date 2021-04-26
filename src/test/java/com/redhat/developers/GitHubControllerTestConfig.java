9cf718db8a85345433bacc1ec2f10651af9d9492
Roberto <robertoyard@gmail.com>
2021-04-26 20:41:47 +0200

    removed unnecessary import

--- src/test/java/com/redhat/developers/GitHubControllerTestConfig.java
+++ src/test/java/com/redhat/developers/GitHubControllerTestConfig.java
@@ -1,6 +1,5 @@
 package com.redhat.developers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
